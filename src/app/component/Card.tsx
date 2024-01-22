36715f613472dcc7cf18e7d2c6b94bc4943e1807
algolkk <loplokeel+gh@gmail.com>
2024-01-22 18:24:51 +0900

    wip

--- src/app/component/Card.tsx
+++ src/app/component/Card.tsx
@@ -1,11 +1,17 @@
 import Icon from "./Icon";
 import Label from "./Label";
+import { IconType } from "react-icons";
+import styles from "./../css/Card.module.css";
 
-export default function Card() {
+interface CardProps {
+    icon: IconType;
+}
+
+export const Card: React.FC<CardProps> = ({ icon }) => {
     return (
-        <div>
-            <Icon />
+        <div className={styles.cardContainer}>
+            <Icon icon={icon} />
             <Label />
         </div>
     );
-}
+};
--- src/app/component/Icon.tsx
+++ src/app/component/Icon.tsx
@@ -1,3 +1,9 @@
-export default function Icon() {
-    return <div>input image here.</div>;
+import { IconType } from "react-icons";
+
+interface IconProps {
+    icon: IconType;
+}
+
+export default function Icon({ icon: IconComponent }: IconProps) {
+    return <IconComponent />;
 }
