a0714584188b3c44880c0555ab948a58ac08c803
algolkk <loplokeel+gh@gmail.com>
2024-01-26 21:27:13 +0900

    Refactor card layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,41 +17,80 @@ export default function Home() {
             style={{
                 display: "flex",
                 flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
+                justifyContent: "space-between",
                 height: "100vh",
             }}
         >
-            <Card
-                icon={FaTwitch}
-                text={"Twitch"}
-                url={"https://www.twitch.tv/"}
-            />
-            <Card
-                icon={SiMisskey}
-                text={"Misskey.io"}
-                url={"https://misskey.io/"}
-            />
-            <Card
-                icon={FaDiscord}
-                text={"Discord"}
-                url={"https://discord.com/"}
-            />
-            <Card
-                icon={FaInstagram}
-                text={"Instagram"}
-                url={"https://www.instagram.com/"}
-            />
-            <Card
-                icon={FaSteam}
-                text={"Steam"}
-                url={"https://store.steampowered.com/"}
-            />
-            <Card
-                icon={FaGithub}
-                text={"Github"}
-                url={"http://github.com/algolkk"}
-            />
+            <div style={{ flex: 1 }}></div>
+            <div
+                style={{
+                    flex: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        width: "100%",
+                    }}
+                >
+                    <Card
+                        icon={FaTwitch}
+                        text={"Twitch"}
+                        url={"https://www.twitch.tv/"}
+                    />
+                    <Card
+                        icon={SiMisskey}
+                        text={"Misskey.io"}
+                        url={"https://misskey.io/"}
+                    />
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        width: "100%",
+                    }}
+                >
+                    <Card
+                        icon={FaDiscord}
+                        text={"Discord"}
+                        url={"https://discord.com/"}
+                    />
+                    <Card
+                        icon={FaInstagram}
+                        text={"Instagram"}
+                        url={"https://www.instagram.com/"}
+                    />
+                </div>
+
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        width: "100%",
+                    }}
+                >
+                    <Card
+                        icon={FaSteam}
+                        text={"Steam"}
+                        url={"https://store.steampowered.com/"}
+                    />
+                    <Card
+                        icon={FaGithub}
+                        text={"Github"}
+                        url={"http://github.com/algolkk"}
+                    />
+                </div>
+            </div>
+            <div style={{ flex: 1 }}></div>
         </div>
     );
 }
